526144dfbf7d17dc04a7c3898299b2572248d952
PraveenPallagani <praveenpallagani999@gmail.com>
2024-10-27 17:50:12 +0530

    Add new roles prop to User model

--- Authentication/admin.py
+++ Authentication/admin.py
@@ -6,13 +6,20 @@ from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
 class CustomUserAdmin(BaseUserAdmin):
     list_display = ('phone_number', 'first_name', 'last_name', 'is_active', 'is_staff')
-    list_filter = ('is_active', 'is_staff')
+    list_filter = ('is_active', 'is_staff','role')
     ordering = ('phone_number', )
 
     fieldsets = (
         (None, {'fields': ('phone_number', 'password')}),
-        ("Personal Info", {'fields': ('first_name', 'last_name')}),
+        ("Personal Info", {'fields': ('first_name', 'last_name','role')}),
         ('Permissions', {'fields': ('is_active','is_staff')})
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('phone_number', 'password1', 'password2', 'first_name', 'last_name', 'role')}
+        ),
     )
     
     def get_form(self, request, obj=None, **kwargs):
--- Authentication/models.py
+++ Authentication/models.py
@@ -3,13 +3,14 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, Permis
 
 # Create your models here.
 class CustomUserManager(BaseUserManager):
-    def create_user(self, phone_number, password, **extra_fields):
+    def create_user(self, phone_number, password, role='admin', **extra_fields):
         # validations
         if not phone_number:
             raise ValueError("Phone number is required")
         if password and len(password) < 4:
             raise ValueError('Password should contain atleast 4 charecters')
         # save the user to db
+        extra_fields.setdefault('role',role)
         user = self.model(phone_number=phone_number,**extra_fields)
         user.set_password(password)
         user.save(using=self._db)
@@ -18,9 +19,18 @@ class CustomUserManager(BaseUserManager):
     def create_superuser(self, phone_number, password, **extra_fields):
         extra_fields.setdefault('is_staff',True)
         extra_fields.setdefault('is_superuser',True)
-        return self.create_user(phone_number=phone_number,password=password,**extra_fields)
+        return self.create_user(phone_number=phone_number,password=password, role='admin', **extra_fields)
     
 class CustomUser(AbstractBaseUser, PermissionsMixin):
+    # roles
+    ADMIN = 'admin'
+    FARMER = 'farmer'
+    CUSTOMER = 'customer'
+    ROLES = [
+        (ADMIN,'Admin'),
+        (FARMER,'Farmer'),
+        (CUSTOMER,'Customer')
+    ]
     # user properties
     phone_number = models.CharField(max_length=15, unique=True)
     first_name = models.CharField(max_length=30)
@@ -28,7 +38,7 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
     # user roles
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
-
+    role = models.CharField(max_length=10,choices=ROLES, default=ADMIN)
     objects = CustomUserManager()
 
     USERNAME_FIELD = 'phone_number'
