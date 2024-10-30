cb4c0acfb7b77112d569e8da0a3d8a11bcfc41c7
PraveenPallagani <praveenpallagani999@gmail.com>
2024-10-30 22:13:45 +0530

    new fields added in product field

--- Farmer/models.py
+++ Farmer/models.py
@@ -2,6 +2,16 @@ from django.db import models
 from django.conf import settings
 from django.core.exceptions import ValidationError
 
+class Category(models.Model):
+    CATEGORIES = [
+        ('vegetables','Vegetables'),
+        ('fruits','Fruits')
+    ]
+    name = models.CharField(max_length=50,choices=CATEGORIES,unique=True)
+
+    def __str__(self):
+        return self.name
+
 # Create your models here.
 class Product(models.Model):
     UNITS = [
@@ -14,11 +24,17 @@ class Product(models.Model):
         on_delete=models.CASCADE,
         related_name='products'
     )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE,
+        related_name='products'
+    )
     # product fields
     name = models.CharField(max_length=100)
     description = models.TextField()
     measure_unit = models.CharField(max_length=10,choices=UNITS)
     quantity = models.PositiveIntegerField()
+    discounted_price = models.DecimalField(max_digits=10,decimal_places=2,null=True)
     price = models.DecimalField(max_digits=10,decimal_places=2)
     # audit fields
     created_at = models.DateTimeField(auto_now_add=True)
