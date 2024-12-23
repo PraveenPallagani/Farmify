4d0b7c5b45ddc433db9cdf8b3cb319a420ab2bf0
PraveenPallagani <praveenpallagani999@gmail.com>
2024-12-23 23:21:01 +0530

    tailwind css issue fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['*'],
+  content: [
+    '*',
+    './templates/**/*.html',
+    './static/**/*.js'
+  ],
   theme: {
     extend: {},
   },
