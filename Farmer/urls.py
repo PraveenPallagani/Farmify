a9f6d41323c8540c7a20f99da488c3abde36a58e
PraveenPallagani <praveenpallagani999@gmail.com>
2024-12-23 12:06:44 +0530

    Photos added for each product

--- Farmer/urls.py
+++ Farmer/urls.py
@@ -3,7 +3,7 @@ from . import views as farmer_view
 
 urlpatterns = [
     path('home',farmer_view.farmer_home,name="Farmer Home"),
-    path('add-product',farmer_view.add_product,name="Add Produt"),
+    path('add-product',farmer_view.add_product,name="Add Product"),
     path('all-products',farmer_view.all_products,name="All Products"),
     path('product-details/<int:id>',farmer_view.product_details,name="Prodyuct Details")
 ]
--- Farmer/views.py
+++ Farmer/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import *
 from Authentication.decorators import role_required
 from django.contrib import messages
-from .models import Product, Category
+from .models import Product, Category, ProductImage
 
 @role_required('farmer')
 def farmer_home(request:HttpRequest):
@@ -19,12 +19,20 @@ def add_product(request):
         product_category = request.POST.get('product_category')
         product_quantity = request.POST.get('product_quantity')
         product_price = request.POST.get('product_price')
+        images = request.FILES.getlist('images')
+        # validate no of images 
+        if not validateImagesCount(request,images):
+            return redirect("Add Product")
         # get the category instance
         category = Category.objects.get(name = product_category)
         # create the prodct object and save
         product = Product.objects.create(user=request.user,category=category,name=product_name,description=product_desc,
             measure_unit=product_measure_unit,quantity=product_quantity,price=product_price)
         product.save()
+        # now create the images
+        for image in images:
+            product_image = ProductImage.objects.create(product=product, image=image)
+            product_image.save()
         # add the success message
         messages.success(request, f"{product} is created successfully by {request.user}")
     # serve the add product form
@@ -43,3 +51,13 @@ def product_details(request,id):
     product = Product.objects.get(id=id)
     context = {'product':product}
     return render(request, 'farmer/product-details.html',context)
+
+
+def validateImagesCount(request:HttpRequest, images:list):
+    if len(images)==0:
+        messages.error(request, 'Atleast one image of product should be upload')
+        return False
+    if len(images)>5:
+        messages.error(request, 'At max only 5 images of product can be upload')
+        return False
+    return True
